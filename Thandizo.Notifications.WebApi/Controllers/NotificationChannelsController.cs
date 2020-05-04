93c1671b5a237e2a9cf06460e5acfd374acf7c92
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-04 10:39:11 +0200

    Added NotificationsChannelController class

---
+++ Thandizo.Notifications.WebApi/Controllers/NotificationChannelsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DataModels.Notifications;
+using Thandizo.Notifications.BLL.Services;
+
+namespace Thandizo.Notifications.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationChannelsController : ControllerBase
+    {
+        INotificationChannelService _service;
+
+        public NotificationChannelsController(INotificationChannelService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] int channelId)
+        {
+            var response = await _service.Get(channelId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetAll")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.Get();
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [ValidateModelState]
+        [CatchException(MessageHelper.AddNewError)]
+        [ValidateModelState]
+        public async Task<IActionResult> Add([FromBody]NotificationChannelDTO notificationChannel)
+        {
+            var outputHandler = await _service.Add(notificationChannel);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler.Message);
+            }
+
+            return Created("", outputHandler.Message);
+        }
+
+        [HttpPut("Update")]
+        [ValidateModelState]
+        [CatchException(MessageHelper.UpdateError)]
+        public async Task<IActionResult> Update([FromBody]NotificationChannelDTO notificationChannel)
+        {
+            var outputHandler = await _service.Update(notificationChannel);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler.Message);
+            }
+
+            return Ok(outputHandler.Message);
+        }
+
+        [HttpDelete("Delete")]
+        [CatchException(MessageHelper.DeleteError)]
+        public async Task<IActionResult> Delete([FromQuery]int channelId)
+        {
+            var outputHandler = await _service.Delete(channelId);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler.Message);
+            }
+
+            return Ok(outputHandler.Message);
+        }
+    }
+}
