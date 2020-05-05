9ef72acd8f86097b291f445c9acbf889993e1031
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-05 11:49:49 +0200

    Added BulkNotificatinLogService and IBulkNotificatinLogService classes

---
+++ Thandizo.Notifications.BLL/Services/BulkNotificationLogService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.DataMapping;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DAL.Models;
+using Thandizo.DataModels.General;
+using Thandizo.DataModels.Notifications;
+
+namespace Thandizo.Notifications.BLL.Services
+{
+    public class BulkNotificationLogService : IBulkNotificationLogService
+    {
+        private thandizoContext _context;
+
+        public BulkNotificationLogService(thandizoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<OutputResponse> Get(long notificationLogId)
+        {
+            var scheduledNotificationLog = await _context.BulkNotificationLog.FirstOrDefaultAsync(x => x.NotificationLogId.Equals(notificationLogId));
+
+            var mappedBulkNotificationLog = new AutoMapperHelper<BulkNotificationLog, BulkNotificationLogDTO>().MapToObject(scheduledNotificationLog);
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = scheduledNotificationLog
+            };
+        }
+
+        public async Task<OutputResponse> GetByNotificationId(long notificationId)
+        {
+            var scheduledNotificationLogs = await _context.BulkNotificationLog.Where(x => x.NotificationId.Equals(notificationId)).OrderBy(x => x.NotificationLogId).ToListAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = scheduledNotificationLogs
+            };
+        }
+        
+        public async Task<OutputResponse> GetByPhoneNumber(string phoneNumber)
+        {
+            var scheduledNotificationLogs = await _context.BulkNotificationLog.Where(x => x.PhoneNumber.Equals(phoneNumber)).OrderBy(x => x.NotificationLogId).ToListAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Result = scheduledNotificationLogs
+            };
+        }
+
+        public async Task<OutputResponse> Add(BulkNotificationLogDTO scheduledNotificationLog)
+        {
+
+            var mappedBulkNotificationLog = new AutoMapperHelper<BulkNotificationLogDTO, BulkNotificationLog>().MapToObject(scheduledNotificationLog);
+
+            mappedBulkNotificationLog.DateCreated = DateTime.UtcNow.AddHours(2);
+
+            await _context.BulkNotificationLog.AddAsync(mappedBulkNotificationLog);
+            await _context.SaveChangesAsync();
+
+            return new OutputResponse
+            {
+                IsErrorOccured = false,
+                Message = MessageHelper.AddNewSuccess
+            };
+        }
+
+    }
+}
---
+++ Thandizo.Notifications.BLL/Services/IBulkNotificationLogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Thandizo.DataModels.General;
+using Thandizo.DataModels.Notifications;
+
+namespace Thandizo.Notifications.BLL.Services
+{
+    public interface IBulkNotificationLogService
+    {
+        Task<OutputResponse> Add(BulkNotificationLogDTO scheduledNotificationLog);
+        Task<OutputResponse> Get(long notificationLogId);
+        Task<OutputResponse> GetByPhoneNumber(string notificationLogId);
+        Task<OutputResponse> GetByNotificationId(long notificationId);
+    }
+}
