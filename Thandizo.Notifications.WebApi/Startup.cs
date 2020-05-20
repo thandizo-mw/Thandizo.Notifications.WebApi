c5e6122753689750d10b1bc68aa047227fbd85fb
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-20 20:54:48 +0200

    Added Swagger to API

--- Thandizo.Notifications.WebApi/Startup.cs
+++ Thandizo.Notifications.WebApi/Startup.cs
@@ -1,12 +1,14 @@
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 using Thandizo.DAL.Models;
 
 namespace Thandizo.Notifications.WebApi
@@ -46,6 +48,18 @@ namespace Thandizo.Notifications.WebApi
             {
                 options.SuppressModelStateInvalidFilter = true;
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Thandizo Notifications API",
+                    Description = "Notifications API for Thandizo platform",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact { Name = "COVID-19 Malawi Tech Response", Email = "thandizo.mw@gmail.com", Url = new Uri("https://www.thandizo.mw") }
+                });
+                c.IncludeXmlComments(GetXmlCommentsPath());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +70,17 @@ namespace Thandizo.Notifications.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            //this is not needed in PRODUCTION but only in Hosted Testing Environment
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Thandizo Notifications API V1");
+            });
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod());
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -66,6 +91,10 @@ namespace Thandizo.Notifications.WebApi
             {
                 endpoints.MapControllers();
             });
+        }
+        private string GetXmlCommentsPath()
+        {
+            return Path.Combine(AppContext.BaseDirectory, "Thandizo.Notifications.WebApi.xml");
         }
     }
 }
